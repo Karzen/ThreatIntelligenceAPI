2c80aef85e60be8ffb041ce5063564c8995cd823
Karzen <alexstefansbr@gmail.com>
2026-03-15 21:32:58 +0200

    Created filestructure

---
+++ ThreatIntelAPI.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace ThreatIntelAPI.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
